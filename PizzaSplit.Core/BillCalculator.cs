052a40082ee0d43dd4d6f9e7e58529e74c70d18e
zakharchenko <zakharchenko@NK-W13-16>
2026-09-07 19:00:52 +0300

    Add project files.

---
+++ PizzaSplit.Core/BillCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace PizzaSplit.Core
+{
+    public class BillCalculator
+    {
+        public double Calculate(int people, double amount, bool tips)
+        {
+            if (tips)
+            {
+                amount = amount * 1.10;
+            }
+
+            double result = amount / people;
+
+            return result;
+        }
+
+    }
+}
---
+++ PizzaSplit.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PizzaSplit.Core;
+namespace PizzaSplit.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_click(object sender, RoutedEventArgs e)
+        {
+            
+            int people = int.Parse(PeopleTextBox.Text);
+
+            if (people < 1 || people > 20)
+            {
+                MessageBox.Show("Max number of people is 1 to 20");
+                return;
+            }
+
+            double amount = double.Parse(AmountTextBox.Text);
+            
+            if (amount < 0 || amount >= 10000)
+            {
+                MessageBox.Show("Max amount is 0 < amount <= 10000");
+                return;
+            }
+
+            bool tips = TipCheckBox.IsChecked == true;
+
+            BillCalculator calculator = new BillCalculator();
+
+            double result = calculator.Calculate(people, amount, tips);
+
+            ResultTextBox.Text = result.ToString();
+        }
+    }
+}
